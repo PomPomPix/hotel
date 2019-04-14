4b434fecaeb1cf800359ded1eef4e3b5079a3b43
Avi Guner <aviguner1@gmail.com>
2019-04-14 12:25:12 +0300

    first commit

--- hotelproject/urls.py
+++ hotelproject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('hotels/', include('hotels.urls')),
+    path('visitors/', include('visitors.urls')),
     path('admin/', admin.site.urls),
 ]
