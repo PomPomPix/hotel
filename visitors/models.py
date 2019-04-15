73988a861bd8c23276e4429ef51883d87fdd7b8e
Avi Guner <aviguner1@gmail.com>
2019-04-15 11:58:35 +0300

    info_request_page

--- visitors/models.py
+++ visitors/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Request(models.Model):
+      Name = models.CharField(max_length = 20)
+      Phone = models.IntegerField()
+      Email = models.EmailField(max_length = 30)  
+      Special_request = models.CharField(max_length = 280)
+      
--- visitors/views.py
+++ visitors/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
+from visitors import forms
+from visitors import models
 from django.http import HttpResponse
+
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+ 
+def Request_page(request):
+
+    form = forms
+    data = models.Contact.objects.all()
+
+    if(request.method=='POST'):
+
+        Name = request.POST['Name']
+        Phone = request.POST['Phone']
+        Email = request.POST['Email']
+        Special_request = request.POST['Special_request']
+       
+        add = models.Contact(Name=Name, Phone=Phone, Email=Email, Special_request=Special_request)      
+        add.save()
+ 
+    return render(request,'request.html',locals())
