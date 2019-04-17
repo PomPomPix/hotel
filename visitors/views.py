7228eb8f047edd014e2b5506ab7f748e3203ffc7
Lea Soussan <leapmdi@gmail.com>
2019-04-17 11:43:09 +0300

    info page and style

--- visitors/views.py
+++ visitors/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+<<<<<<< Updated upstream
 from django.contrib.auth.decorators import login_required
+=======
+<<<<<<< Updated upstream
+=======
+from django.contrib.auth.decorators import login_required
+from .models import Reservation
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 
 def index(request):
@@ -10,12 +18,32 @@ def index(request):
 def info_page(request):
 	info_hotel = "this is a 5 Stars Hotel, where you get to come in and relax. Click herre for more details" 
 	context = {
-	'info_hotel': info_hotel
+	'info_hotel': info_hotel,
 	}
 	
 	return render(request, 'info.html', context)
+<<<<<<< Updated upstream
 
 
 # @login_required
 # def make_reservation(request):
 # 	pass	
+=======
+<<<<<<< Updated upstream
+	
+=======
+
+
+def booking(request):
+	free_room = Reservation.objects.all()
+
+	context= {
+
+	}
+	return render(request, 'booking.html', context)
+
+# @login_required
+# def make_reservation(request):
+# 	pass	
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
